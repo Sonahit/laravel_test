8700482a43e8bd782a44f18189ffe72383ee6695
Sonahit <grandpajok@gmail.com>
2019-10-23 16:24:35 +0600

    Seeding is not working perfectly

--- database/seeds/BilledMealsSeeder.php
+++ database/seeds/BilledMealsSeeder.php
@@ -18,16 +18,14 @@ class BilledMealsSeeder extends Seeder
         $billed_meals_info = Billed_Meals::withoutGlobalScope('january_business')
             ->select($rows)
             ->groupBy('nomenclature')
-            ->get()
-        ;
+            ->get();
         DatabaseHelper::updateOrInsert('billed_meals_info', $billed_meals_info, $rows);
         info('Done');
         info('Started seeding billed_meals_prices');
         $rows = ['name', 'delivery_number', 'qty' ,'price_per_one', 'total', 'total_novat_discounted'];
         $billed_meals_price = Billed_Meals::withoutGlobalScope('january_business')
             ->select($rows)->groupBy('name', 'price_per_one', 'total')
-            ->get()
-        ;
+            ->get();
         DatabaseHelper::updateOrInsert('billed_meals_prices', $billed_meals_price, $rows);
         info('Done');
     }
