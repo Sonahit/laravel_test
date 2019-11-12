dd74b60217a2b3df9e3cfe6bc6ec5f9d2bef3922
Иван Садыков <grandpajok@gmail.com>
2019-11-12 13:33:37 +0600

    Fixed routing test

--- resources/tests/Routing.test.js
+++ resources/tests/Routing.test.js
@@ -11,12 +11,12 @@ const linkSelector = href => `a[href="${href}"]`;
 test("should navigate to /import", () => {
     const { container } = render(<App />);
     fireEvent.click(document.querySelector(linkSelector("/import")));
-    expect(container.innerHTML).toMatch("Choose file");
+    expect(location.href).toMatch("import");
 });
 
 test("should navigate from /import to /", () => {
     const { container } = render(<App />);
     fireEvent.click(document.querySelector(linkSelector("/import")));
     fireEvent.click(document.querySelector(linkSelector("/")));
-    expect(container.innerHTML).toMatch("filtering__select");
+    expect(location.href).not.toMatch("/import");
 });
