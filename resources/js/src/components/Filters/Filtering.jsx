de967fb9f6fe233269cdca763d3b7a7f32ec52d5
Sonahit <grandpajok@gmail.com>
2019-11-19 14:24:35 +0600

    updated .gitignore

--- resources/js/src/components/Filters/Filtering.jsx
+++
@@ -1,5 +0,0 @@
-import DateFiltering from './DateFiltering';
-import NumberFiltering from './NumberFiltering';
-import StringFiltering from './StringFiltering';
-
-export { DateFiltering, NumberFiltering, StringFiltering };
