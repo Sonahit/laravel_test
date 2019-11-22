ae9458ae2fbf0327eb00c7700ac9a0b11012f886
Иван Садыков <grandpajok@gmail.com>
2019-11-22 16:50:05 +0600

    Changed to collection function

--- app/Collections/Billed_Meals_Collection.php
+++ app/Collections/Billed_Meals_Collection.php
@@ -8,32 +8,40 @@ class Billed_Meals_Collection extends Collection{
 
   public function formatByDate(){
     return $this->map(function($items){
-      $new_collection = $items
+      return $items
           ->groupBy("flight_date")
-          ->map(function($values_at_date){
-              $accum = [];
-              $accum["fact_attributes"] = [];
-              $fact = &$accum["fact_attributes"];
-              $fact["qty"] = 0;
-              $fact["price"] = 0;
-              $fact["codes"] = [];
-              foreach ($values_at_date as $value) {
-                  if(!array_key_exists("id", $accum)) $accum["id"] = $value->flight_id;
-                  if(!array_key_exists("date", $accum)) $accum["date"] = $value->flight_date;
-                  if(!array_key_exists("class", $accum)) $accum["class"] = $value->class;
-                  if(!array_key_exists("type", $accum)) $accum["type"] = $value->type;                                
-                  $accum["plan_attributes"] = $value->new_matrix->groupAndSum();
-                  $billed_prices = $value->billed_meals_prices;
-                  $iata_code = $value->billed_meals_info->iata_code;
-                  $fact["qty"] += $billed_prices->qty;
-                  $fact["price"] += $billed_prices->qty * $billed_prices->price_per_one;
-                  if(!in_array($iata_code, $fact["codes"])) array_push($fact["codes"], $iata_code);
-              }
-              return $accum; 
+          ->map(function($valuesByDate){
+            return $valuesByDate->reduce(function($accum, $value){
+                $fact = &$accum["fact_attributes"];
+                if(is_null($accum["id"])) $accum["id"] = $value->flight_id;
+                if(is_null($accum["date"])) $accum["date"] = $value->flight_date;
+                if(is_null($accum["class"])) $accum["class"] = $value->class;
+                if(is_null($accum["type"])) $accum["type"] = $value->type;                                
+                $accum["plan_attributes"] = $value->new_matrix->groupAndSum();
+                $billed_prices = $value->billed_meals_prices;
+                $iata_code = $value->billed_meals_info->iata_code;
+                $fact["qty"] += $billed_prices->qty;
+                $fact["price"] += $billed_prices->qty * $billed_prices->price_per_one;
+                if(!in_array($iata_code, $fact["codes"])) array_push($fact["codes"], $iata_code);
+                return $accum;
+              }, [
+                "id" => null,
+                "date" => null,
+                "class" => null,
+                "type" => null,
+                "fact_attributes" => [
+                  "qty" => 0,
+                  "codes" => [],
+                  "price" => 0 
+                ],
+                "plan_attributes" => [
+                  "qty" => 0,
+                  "codes" => [],
+                  "price" => 0 
+                ]
+            ]);
           });
-      return $new_collection;
-    });
+      });
+    }
   }
-  
-}
 ?>
