8db1c503b8f4cfeed124f98d96ef900d4d3feb73
Иван Садыков <grandpajok@gmail.com>
2019-12-05 18:45:01 +0600

    Code formatting PSR1 PSR12\PSR2

--- tests/Unit/ApiTest.php
+++ tests/Unit/ApiTest.php
@@ -3,34 +3,36 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+
 class ApiTest extends TestCase
 {
     private $apiPath = "/api/v1";
-    
+
 
     public function testShouldGetReport()
     {
-      $resp = $this->get("{$this->apiPath}/billed_meals");
-      $resp->assertOk();
-      $json = $resp->json();
-      $resp->assertJson($json);
-      $this->assertNotEmpty($json);
+        $resp = $this->get("{$this->apiPath}/billed_meals");
+        $resp->assertOk();
+        $json = $resp->json();
+        $resp->assertJson($json);
+        $this->assertNotEmpty($json);
     }
 
     public function testShouldbeEmpty()
     {
-      $resp = $this->get("{$this->apiPath}/billed_meals?page=2&paginate=-1");
-      $resp->assertStatus(200);
-      $data = json_decode($resp->baseResponse->getContent())->pages;
-      $this->assertEmpty($data);
+        $resp = $this->get("{$this->apiPath}/billed_meals?page=2&paginate=-1");
+        $resp->assertStatus(200);
+        $data = json_decode($resp->baseResponse->getContent())->pages;
+        $this->assertEmpty($data);
     }
 
-    public function testShouldNotGetPDF(){
-      $this->post("{$this->apiPath}/pdf")->assertStatus(200);
+    public function testShouldNotGetPDF()
+    {
+        $this->post("{$this->apiPath}/pdf")->assertStatus(200);
     }
 
-    public function testShouldGetPDF(){
-      $this->get("{$this->apiPath}/pdf")->assertOk();
+    public function testShouldGetPDF()
+    {
+        $this->get("{$this->apiPath}/pdf")->assertOk();
     }
-
 }
